a8e4242082e79b222799f2e94be7563ed75b0d80
chadluo <i@yuki.rocks>
2016-04-01 23:51:55 +1100

    custom fonts

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-// document.getElementById('font1').innerHTML =
-//     document.getElementById('font2').innerHTML;
-// document.getElementById('font2').addEventListener('keyup', function() {
-//   document.getElementById('font1').innerHTML =
-//       document.getElementById('font2').innerHTML;
-// });
-
 $('#font1').html($('#input').val())
 $('#font2').html($('#input').val())
 $('#input')
     .keyup(function() {
-      $('#font1').html($('#input').val())
-      $('#font2').html($('#input').val())
+      $('#font1').html($(this).val())
+      $('#font2').html($(this).val())
     })
+
+$('#fontfamily1')
+    .keyup(function() {
+      $('#font1').css('font-family', $(this).val());
+      $('#input').css('font-family', $(this).val());
+    });
+
+$('#fontfamily2')
+    .keyup(function() {
+      $('#font2').css('font-family', $(this).val());
+    });
