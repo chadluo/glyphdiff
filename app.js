ae51229dea112ef20065cad2adaa005b2fee9609
chadluo <i@yuki.rocks>
2016-04-02 13:08:36 +1100

    rip off jquery/bootstrap dependency

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 // sync sample content
 
-$('#font1').html($('#input').val())
-$('#font2').html($('#input').val())
-$('#input')
-    .keyup(function() {
-      $('#font1').html($(this).val())
-      $('#font2').html($(this).val())
-    })
+var f1 = document.getElementById('font1'),
+    f2 = document.getElementById('font2'),
+    input = document.getElementById('input')
+
+f1.textContent = input.value
+f2.textContent = input.value
+input.addEventListener('keyup', function() {
+  f1.textContent = input.value
+  f2.textContent = input.value
+});
 
 // listen font family
 
-$('#fontfamily1')
-    .keyup(function() {
-      $('#font1').css('font-family', $(this).val());
-      $('#input').css('font-family', $(this).val());
-    });
+var family1 = document.getElementById('fontfamily1'),
+    family2 = document.getElementById('fontfamily2')
 
-$('#fontfamily2')
-    .keyup(function() {
-      $('#font2').css('font-family', $(this).val());
-    });
+family1.addEventListener('keyup', function() {
+  f1.style.fontFamily = family1.value
+  input.style.fontFamily = family1.value
+})
+
+family2.addEventListener(
+    'keyup', function() { f2.style.fontFamily = family2.value });
 
 // listen font weight
 
-$('#fontweight1')
-    .change(function() {
-      $('#font1').css('font-weight', $(this).val());
-    });
+var weight1 = document.getElementById('fontweight1'),
+    weight2 = document.getElementById('fontweight2')
 
-$('#fontweight2')
-    .change(function() {
-      $('#font2').css('font-weight', $(this).val());
-    });
+weight1.addEventListener(
+    'change', function() { f1.style.fontWeight = weight1.value })
+
+weight2.addEventListener(
+    'change', function() { f2.style.fontWeight = weight2.value })
