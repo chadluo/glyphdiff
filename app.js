e7c9f6665cdb64ee5ca8c8deab6d50b1afee864a
chadluo <i@yuki.rocks>
2016-04-09 02:56:24 +1000

    simple refactor

--- app.js
+++ app.js
@@ -2,46 +2,41 @@
 /*jslint node:true*/
 'use strict';
 
-/// id-selector shorthand
+/// shorthands
 
-function $(x) {
-  return document.getElementById(x);
-}
+function $(x) { return document.getElementById(x); } // id selector
+function l(t, e, c) { t.addEventListener(e, c); }    // event listener
 
 // init
 
 var s1 = $('sample1'), s2 = $('sample2');
 s1.textContent = s2.textContent = $('input').value;
 s1.setAttribute('font-family', 'Tsukushi A Round Gothic');
+$('input').style.fontFamily = 'Tsukushi A Round Gothic';
 s2.setAttribute('font-family', 'Tsukushi B Round Gothic');
 s2.setAttribute('fill', 'green');
 s2.setAttribute('fill-opacity', '.7');
 
 // update sample content
 
-$('input')
-    .addEventListener(
-        'input', function() { s1.textContent = s2.textContent = this.value; });
+l($('input'), 'input',
+  function() { s1.textContent = s2.textContent = this.value; });
 
 // update font family
 
-$('fontfamily1')
-    .addEventListener('input', function() {
-      $('input').style.fontFamily = this.value || 'Tsukushi A Round Gothic';
-      s1.setAttribute('font-family', this.value || 'Tsukushi A Round Gothic');
-    });
+l($('fontfamily1'), 'input', function() {
+  $('input').style.fontFamily = this.value || 'Tsukushi A Round Gothic';
+  s1.setAttribute('font-family', this.value || 'Tsukushi A Round Gothic');
+});
 
-$('fontfamily2')
-    .addEventListener('input', function() {
-      s2.setAttribute('font-family', this.value || 'Tsukushi B Round Gothic');
-    });
+l($('fontfamily2'), 'input', function() {
+  s2.setAttribute('font-family', this.value || 'Tsukushi B Round Gothic');
+});
 
 // update font color
 
 function updatePageColor(buttonID, bodyClass) {
-  $(buttonID)
-      .addEventListener(
-          'click', function() { document.body.className = bodyClass; });
+  l($(buttonID), 'click', function() { document.body.className = bodyClass; });
 }
 
 updatePageColor('select-light', 'color-light');
@@ -49,58 +44,45 @@ updatePageColor('select-sepia', 'color-sepia');
 updatePageColor('select-storm', 'color-storm');
 updatePageColor('select-dark', 'color-dark');
 
-$('color2')
-    .addEventListener('input', function() {
-      s2.setAttribute('fill', this.value || 'green');
-    });
+l($('color2'), 'input',
+  function() { s2.setAttribute('fill', this.value || 'green'); });
 
 // update slant
 
-$('slant')
-    .addEventListener('change', function() {
-      s1.setAttribute('font-style', this.value);
-      s2.setAttribute('font-style', this.value);
-    });
+l($('slant'), 'change', function() {
+  s1.setAttribute('font-style', this.value);
+  s2.setAttribute('font-style', this.value);
+});
 
 // update font weight
 
-$('fontweight1')
-    .addEventListener(
-        'change', function() { s1.setAttribute('font-weight', this.value); });
+l($('fontweight1'), 'change',
+  function() { s1.setAttribute('font-weight', this.value); });
 
-$('fontweight2')
-    .addEventListener(
-        'change', function() { s2.setAttribute('font-weight', this.value); });
+l($('fontweight2'), 'change',
+  function() { s2.setAttribute('font-weight', this.value); });
 
 // upate font size
 
-$('size2')
-    .addEventListener('input', function() {
-      s2.setAttribute('font-size', this.value * 50 || 50);
-    });
+l($('size2'), 'input',
+  function() { s2.setAttribute('font-size', this.value * 50 || 50); });
 
 // update varter spacing
 
-$('spacing2')
-    .addEventListener('input', function() {
-      s2.setAttribute('letter-spacing', this.value || 0);
-    });
+l($('spacing2'), 'input',
+  function() { s2.setAttribute('letter-spacing', this.value || 0); });
 
 // update opacity
 
-$('opacity2')
-    .addEventListener('input', function() {
-      s2.setAttribute('fill-opacity', this.value || 0.7);
-    });
+l($('opacity2'), 'input',
+  function() { s2.setAttribute('fill-opacity', this.value || 0.7); });
 
 // load webfonts
 
 var h = document.head.innerHTML;
 
-$('webfont1')
-    .addEventListener(
-        'input', function() { document.head.innerHTML = h + this.value; });
+l($('webfont1'), 'input',
+  function() { document.head.innerHTML = h + this.value; });
 
-$('webfont2')
-    .addEventListener(
-        'input', function() { document.head.innerHTML = h + this.value; });
+l($('webfont2'), 'input',
+  function() { document.head.innerHTML = h + this.value; });
