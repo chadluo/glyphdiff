c7aea2f4483b53bd1574a0e8cb80d3d99923db1c
chadluo <i@yuki.rocks>
2016-05-21 21:54:27 +1000

    Refactor UI.

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-/*jslint browser:true*/
-/*jslint node:true*/
 'use strict';
 
 var def = {
@@ -8,7 +6,8 @@ var def = {
   fontfamily2: 'Tsukushi B Round Gothic',
   fontweight1: 400,
   fontweight2: 400,
-  slant: 'normal',
+  slant1: 'normal',
+  slant2: 'normal',
   baseline: 1,
   darkmode: 0,
   color: '#008000',
@@ -41,7 +40,6 @@ $('gd-input').oninput = function () {
 s1.style.fontFamily = conf.fontfamily1;
 $('gd-fontfamily1').value = (conf.fontfamily1 == def.fontfamily1 ? '' : conf.fontfamily1);
 s1.style.fontWeight = $('gd-fontweight1').value = conf.fontweight1;
-$('fontweight1-tooltip').textContent = 'Weight:' + conf.fontweight1;
 
 updateLabel('label-font1', s1);
 
@@ -53,16 +51,17 @@ $('gd-fontfamily1').oninput = function () {
 
 $('gd-fontweight1').oninput = function () {
   s1.style.fontWeight = conf.fontweight1 = this.value;
-  $('fontweight1-tooltip').textContent = 'Weight:' + this.value;
+  loadFonts();
   updateLabel('label-font1', s1);
 };
 
 s2.style.fontFamily = conf.fontfamily2;
 $('gd-fontfamily2').value = (conf.fontfamily2 == def.fontfamily2 ? '' : conf.fontfamily2);
 s2.style.fontWeight = $('gd-fontweight2').value = conf.fontweight2;
-$('fontweight2-tooltip').textContent = 'Weight:' + conf.fontweight2;
 
 updateLabel('label-font2', s2);
+
+loadFonts();
 
 $('gd-fontfamily2').oninput = function () {
   s2.style.fontFamily = conf.fontfamily2 = this.value || def.fontfamily2;
@@ -72,24 +71,34 @@ $('gd-fontfamily2').oninput = function () {
 
 $('gd-fontweight2').oninput = function () {
   s2.style.fontWeight = conf.fontweight = this.value;
-  $('fontweight2-tooltip').textContent = 'Weight:' + this.value;
+  loadFonts();
   updateLabel('label-font2', s2);
 };
 
 // slantness
 
-var slantOptions = document.getElementsByName('slant');
+var slantOptions1 = document.getElementsByName('slant1'),
+  slantOptions2 = document.getElementsByName('slant2');
 for (var i = 0; i < 3; i++) {
-  var curr = slantOptions[i];
-  if (curr.value == conf.slant) {
-    curr.checked = true;
-    s1.style.fontStyle = curr.value;
-    s2.style.fontStyle = curr.value;
+  var curr1 = slantOptions1[i],
+    curr2 = slantOptions2[i];
+  if (curr1.value == conf.slant1) {
+    curr1.checked = true;
+    s1.style.fontStyle = curr1.value;
   }
-  curr.onclick = function () {
+  if (curr2.value == conf.slant2) {
+    curr2.checked = true;
+    s2.style.fontStyle = curr2.value;
+  }
+  curr1.onclick = function () {
     s1.style.fontStyle = this.value;
+    conf.slant1 = this.value;
+    loadFonts();
+  };
+  curr2.onclick = function () {
     s2.style.fontStyle = this.value;
-    conf.slant = this.value;
+    conf.slant2 = this.value;
+    loadFonts();
   };
 }
 
@@ -119,20 +128,17 @@ $('gd-darkmode').onchange = function () {
 
 // color
 
-s2.style.fill = $('label-color2').style.fill = conf.color;
-$('gd-color2').value = conf.color;
+s2.style.fill = $('label-color2').style.fill = $('gd-color2').value = conf.color;
 $('gd-color2').oninput = function () {
   s2.style.fill = $('label-color2').style.fill = conf.color = this.value || def.color;
 };
 
 // opacity
 
-s2.style.fillOpacity = $('gd-opacity2').value = conf.opacity;
-$('opacity2-tooltip').textContent = 'Opacity:' + conf.opacity;
-
+s2.style.fillOpacity = $('gd-opacity2').value = $('opacity2-tooltip').textContent = conf.opacity;
 $('gd-opacity2').oninput = function () {
   s2.style.fillOpacity = this.value;
-  $('opacity2-tooltip').textContent = 'Opacity:' + this.value;
+  $('opacity2-tooltip').textContent = this.value;
   conf.opacity = this.value;
 };
 
@@ -140,11 +146,11 @@ $('gd-opacity2').oninput = function () {
 
 s2.style.fontSize = conf.size * 45 + 'vh';
 $('gd-size2').value = conf.size;
-$('size2-tooltip').textContent = 'Relative size:' + conf.size;
+$('size2-tooltip').textContent = conf.size;
 
 $('gd-size2').oninput = function () {
   s2.style.fontSize = this.value * 45 + 'vh';
-  $('size2-tooltip').textContent = 'Relative size:' + this.value;
+  $('size2-tooltip').textContent = this.value;
   conf.size = this.value;
 };
 
@@ -152,11 +158,11 @@ $('gd-size2').oninput = function () {
 
 s2.setAttribute('letter-spacing', conf.tracking + 'em');
 $('gd-spacing2').value = conf.tracking;
-$('spacing2-tooltip').textContent = 'Letter Spacing:' + conf.tracking;
+$('spacing2-tooltip').textContent = conf.tracking;
 
 $('gd-spacing2').oninput = function () {
   s2.setAttribute('letter-spacing', this.value + 'em');
-  $('spacing2-tooltip').textContent = 'Letter spacing:' + this.value;
+  $('spacing2-tooltip').textContent = this.value;
   conf.tracking = this.value;
 };
 
@@ -166,19 +172,19 @@ $('gd-translateX').value = conf.x;
 $('gd-translateY').value = conf.y;
 translate();
 
-$('translateX-tooltip').textContent = 'Translate-x:' + conf.x;
-$('translateY-tooltip').textContent = 'Translate-y:' + conf.y;
+$('translateX-tooltip').textContent = conf.x;
+$('translateY-tooltip').textContent = conf.y;
 
 $('gd-translateX').oninput = function () {
   conf.x = this.value;
   translate();
-  $('translateX-tooltip').textContent = 'Translate-x:' + conf.x;
+  $('translateX-tooltip').textContent = conf.x;
 }
 
 $('gd-translateY').oninput = function () {
   conf.y = this.value;
   translate();
-  $('translateY-tooltip').textContent = 'Translate-y:' + conf.y;
+  $('translateY-tooltip').textContent = conf.y;
 }
 
 // share config snackbar
@@ -213,7 +219,11 @@ function $(x) {
 
 function loadFonts() {
   WebFont.load({
-    google: { families: [conf.fontfamily1, conf.fontfamily2] }
+    google: {
+      families: [conf.fontfamily1 + ':' + conf.fontweight1 + (conf.slant1 == 'normmal' ? '' : conf.slant1),
+        conf.fontfamily2 + ':' + conf.fontweight2 + (conf.slant2 == 'normmal' ? '' : conf.slant2)
+      ]
+    }
   });
 }
 
